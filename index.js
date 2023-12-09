0da8af331a98fffce1c4f381d7aca9be1003eef3
KARTHIK <karthikvasudev97@gmail.com>
2023-12-09 11:46:28 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+let imgbox = document.getElementById("imgbox");
+let qrImage = document.getElementById("qrImage");
+let  qrText = document.getElementById("qrText");
+
+function gererateQr(){
+  if(qrText.value.length >0 ){
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgbox.classList.add("show-img");
+  }else{
+    qrText.classList.add('error');
+    setTimeout(()=>{
+        alert("You Fool🐵 Please Provide Text or URL ");
+        qrText.classList.remove('error');
+        
+    },1000);
+  }
+    
+    
+
+}
